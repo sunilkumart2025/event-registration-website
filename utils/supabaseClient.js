771608821b624f8234d9dd7d298bcbf9f06cf130
sunilkumar_25 <suniltamil2025@gmail.com>
2025-03-27 15:16:36 +0530

    Update supabaseClient.js

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://your-supabase-url.supabase.co"; // Replace with your Supabase URL
-const supabaseAnonKey = "your-anon-key"; // Replace with your Supabase Key
+const supabaseUrl = "https://bbxondugevthazwvltmt.supabase.co"; // Replace with your Supabase URL
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJieG9uZHVnZXZ0aGF6d3ZsdG10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4MTA1OTIsImV4cCI6MjA1ODM4NjU5Mn0.9GflPODSw1CPl-3VaNj4fUCoresMH3HaX4dUcEifaZo"; // Replace with your Supabase Key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
